78d3456f4bb95de232b304ecdd729bf33ce2ec72
Natalia Sokolova <solta@inbox.ru>
2022-08-21 14:20:12 +0300

    14 пр правка 2

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,12 @@
 const router = require('express').Router();
 
 const {
-  getUsers, createUser, getUser, patchUser, patchAvatar,
+  getUsers, getUserById, getUser, patchUser, patchAvatar,
 } = require('../controllers/users');
 const { validateUserId, validateUpdateUser, validateUpdateAvatar } = require('../middlewares/validator');
 
 router.get('/', getUsers);
-router.post('/', createUser);
-router.get('/:userId', validateUserId, getUser);
+router.get('/:userId', validateUserId, getUserById);
 router.get('/me', getUser);
 router.patch('/me', validateUpdateUser, patchUser);
 router.patch('/me/avatar', validateUpdateAvatar, patchAvatar);
